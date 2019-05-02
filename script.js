770d90e1f1b32ad21e12250b1a7f9daa0dc71ad6
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2019-05-02 19:21:29 +0300

    gonna

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+
+let burger = document.getElementById("burger");
+let menu =document.querySelector(".menu-wrap");
+
+function burgerClick()  {
+    if (burger.classList.contains('active')) {
+        burger.classList.remove('active');
+        menu.style.transform = 'translateX(-160px)';
+        }
+        else {
+        burger.classList.add('active');
+        menu.style.transform = "translateX(0px)";
+        }
+}
+
+
+burger.addEventListener('click', burgerClick) ;
+
+}

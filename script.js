15c5372a2495d2f429420290d764db2138a268bb
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2019-05-12 21:19:35 +0300

    all swipers are fully functional on mobile

--- script.js
+++ script.js
@@ -21,9 +21,45 @@ burger.addEventListener('click', burgerClick) ;
 
 }
 
+// //suzinoti dabartinio slide index
+// let currentSlide = swiper2.activeIndex; 
 
-//uzdeti indexus ant pagination, kad jie sutaptu su slide'ais
+// //suzinoti aktyvu button
+// let currentButton = document.querySelector(".features-button-active");
 
-//Pagal dabartinį active pagination atidaryti slide su slideTo funkcija
+// //nustatyti dabartini slide pagal active button
+// //Suzinom aktyvaus mygtuko index ir aktyvinam tokio paties index slide
 
-//
+// let activeIndex = currentButton.getAttribute("index");
+// window.onload=function(){    
+//     swiper2.slideTo(activeIndex, 300);
+// }
+
+// //paspaudus mygtuka aktyvinam ji ir nuvarom i jo slide
+// let buttons = document.querySelectorAll(".features-button");
+// //visiems mygtukams uzdedam paspaudima
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("mousedown", function() {
+//         //jei jau aktyvus, tai nieko nedarom
+//         if (this.classList.contains(".features-button-active")) {}
+//         else {
+//             //visiems nuimam active
+//             for (let j = 0; j < buttons.length; j++) {
+//                 buttons[j].classList.remove(".features-button-active")
+//             }
+//             //uzdedam active
+//             this.classList.add(".features-button-active");
+//             //ijungiam atitinkama slide
+//             swiper2.slideTo(activeIndex, 300);
+//         }
+//     })
+// }
+
+
+//Pagal dabartinį active mygtuka atidaryti slide su slideTo funkcija
+// let slideNumber = this.getAttribute("data-slide");
+//       mySSwiper.slideTo(slideNumber-1, 300);
+
+//Paspaudus ant pagination atidaryti atitinkamą slide
+
+//nudraginus slide pazymeti atitinkama jo pagination.

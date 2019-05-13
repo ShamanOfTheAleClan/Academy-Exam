42d4588b355168c24b0bce7960bfdf83be91109e
ShamanOfTheAleClan <paulius.sviackevicius@gmail.com>
2019-05-13 21:31:28 +0300

    added missing max-width. Added some media queries

--- script.js
+++ script.js
@@ -19,67 +19,25 @@ burger.addEventListener('click', burgerClick) ;
 }
 
 //form validation
-function formValidation()                                    
-{ 
-    let name = document.forms["singUp"]["name"];               
-    let surname = document.forms["signUp"]["surname"];
-    let phone = document.forms["singUp"]["phone"];  
+// function formValidation(){
+
+//     let name = document.forms["singUp"]["name"];               
+//     let surname = document.forms["signUp"]["surname"];
+//     let phone = document.forms["singUp"]["phone"];  
    
-    if (name.value == "")                                  
-    { 
-        window.alert("Please enter your name."); 
-        name.focus(); 
-        return false; 
-    } 
-   
-    if (address.value == "")                               
-    { 
-        window.alert("Please enter your address."); 
-        name.focus(); 
-        return false; 
-    } 
-       
-    if (email.value == "")                                   
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-   
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-   
-    if (email.value.indexOf(".", 0) < 0)                 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
-   
-    if (phone.value == "")                           
-    { 
-        window.alert("Please enter your telephone number."); 
-        phone.focus(); 
-        return false; 
-    } 
-   
-    if (password.value == "")                        
-    { 
-        window.alert("Please enter your password"); 
-        password.focus(); 
-        return false; 
-    } 
-   
-    if (what.selectedIndex < 1)                  
-    { 
-        alert("Please enter your course."); 
-        what.focus(); 
-        return false; 
-    } 
-   
-    return true; 
-}
+//     if (name.value == "")                                  
+//     { 
+//         window.alert("Please enter your name."); 
+//         console.log("something happened");
+//         // name.focus(); 
+//         return false; 
+//     } 
+
+//     if (phone.value == "")                           
+//     { 
+//         window.alert("Please enter your telephone number."); 
+//         // phone.focus(); 
+//         return false; 
+//     } 
+//     return true; 
+// }
